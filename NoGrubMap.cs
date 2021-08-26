9a629331bb3085b34792275ba082c26fa4267491
Jo X <39098450+Tamijo-Netrunner@users.noreply.github.com>
2021-08-26 15:42:11 +0100

    Add files via upload
    Initial creation.

---
+++ NoGrubMap.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine.UI;
+using Modding;
+
+
+namespace NoGrubMap
+{
+    /// <summary>
+    /// Disables Collector's Map. Can be disabled from mod menu.
+    /// </summary>
+    public class NoGrubMap : Mod, ITogglableMod
+    {
+
+        public override string GetVersion() => "1.0";
+
+        // Hook to GetPlayerBoolHook
+        public override void Initialize() {
+            Log("Disabling Collector's Map");
+            ModHooks.Instance.GetPlayerBoolHook += PlayerBoolGet;
+        }
+
+        // Unhook from GetPlayerBoolHook
+        public void Unload() {
+            Log("Allowing Collector's Map");
+            ModHooks.Instance.GetPlayerBoolHook -= PlayerBoolGet;
+        }
+
+        // Overrides Collector's Map bool gets.
+        public bool PlayerBoolGet(string target) {
+
+            switch (target) {
+                // Prevent game from reading Collector's Map
+                case "hasPinGrub":
+                    return false;
+                
+                // Allow other bools through
+                default:
+                    return PlayerData.instance.GetBoolInternal(target);
+            }            
+        }
+    }
+}
